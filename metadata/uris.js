5db62c3cb5c2c09d9bc8a8929eb3880f794626c8
Nicolás Acosta <nicacosta@frba.utn.edu.ar>
2022-04-05 22:45:54 -0300

    add metadata

---
+++ metadata/uris.js
@@ -0,0 +1,10 @@
+module.exports = {
+	collection: {
+		media: 'bafybeihhjsx33w4cogcmljgihlbk26wnsxtlmmrekiiaxiyl3u3ppkek3e',
+		json: 'bafybeiglpyusn3g7umqsemfyb3ey2bfpanzusvpa3sirwdt43kbmo7tofm'
+	},
+	token: {
+		media: 'bafybeigxu7flbzddrcsfrai2yi3vamcmxq7e5ovks7duigwbnbywypcwxm',
+		json: 'bafybeiehchwkoo5lkfckp2nts2zdudvywb4z4ge36cu2y2wsgnnn4wbkya'
+	}
+}
